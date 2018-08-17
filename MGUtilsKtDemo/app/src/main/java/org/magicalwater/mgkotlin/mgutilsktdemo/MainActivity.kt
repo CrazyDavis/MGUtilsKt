3b6e4ba3de4d1a17a1308b312fda5d4d02d25ab5
Water <crazydennies@gmail.com>
2018-08-17 14:04:09 +0800

    0.1.1: 修改 圖片載入工具 MGImgUtils 傳入參數

--- MGUtilsKtDemo/app/src/main/java/org/magicalwater/mgkotlin/mgutilsktdemo/MainActivity.kt
+++ MGUtilsKtDemo/app/src/main/java/org/magicalwater/mgkotlin/mgutilsktdemo/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MGImgLoadUtils.load(mImgView, R.drawable.ic_demo2, MGImgLoadUtils.Shape.NORMAL, 64.0f)
+        MGImgLoadUtils.load(mImgView, R.drawable.ic_demo2, MGImgLoadUtils.ImageAttr(MGImgLoadUtils.Shape.ROUND, 64f))
     }
 }
--- MGUtilsKtDemo/mg-utils-kt/src/main/java/org/magicalwater/mgkotlin/mgutilskt/util/MGImgLoadUtils.kt
+++ MGUtilsKtDemo/mg-utils-kt/src/main/java/org/magicalwater/mgkotlin/mgutilskt/util/MGImgLoadUtils.kt
@@ -28,52 +28,30 @@ class MGImgLoadUtils {
 
     companion object {
 
-        fun load(context: Context, @DrawableRes resId: Int, shape: Shape = Shape.NORMAL, radius: Float? = null, width: Int? = null, height: Int? = null, handler: (Bitmap) -> Unit) {
+        /**
+         * @param from - 可傳入 drawable resId, URL, String, Uri, 分別代表從資源/網路/網路/檔案拉圖
+         * */
+        fun loadBitmap(context: Context, from: Any, attr: ImageAttr, handler: (Bitmap) -> Unit) {
             val target = object: SimpleTarget<Bitmap>() {
                 override fun onResourceReady(resource: Bitmap?, glideAnimation: GlideAnimation<in Bitmap>?) {
                     if (resource != null) handler(resource)
                 }
             }
-            loadBitmapByGlide(context, resId, getTransform(context, shape, radius), width, height)?.into(target)
+            loadBitmapByGlide(context, from, attr)?.into(target)
         }
 
-        fun load(context: Context, resId: Int, shape: Shape = Shape.NORMAL, radius: Float? = null, handler: (GlideDrawable) -> Unit) {
+        fun loadDrawable(context: Context, from: Any, attr: ImageAttr, handler: (GlideDrawable) -> Unit) {
             val target = object: SimpleTarget<GlideDrawable>() {
                 override fun onResourceReady(resource: GlideDrawable?, glideAnimation: GlideAnimation<in GlideDrawable>?) {
                     if (resource != null) handler(resource)
                 }
             }
-            loadDrawableByGilde(context, resId, getTransform(context, shape, radius))?.into(target)
+            loadDrawableByGilde(context, from, attr)?.into(target)
         }
 
         //直接將資源圖片載入進 imageView
-        fun load(view: ImageView, @DrawableRes resId: Int, shape: Shape = Shape.NORMAL, radius: Float? = null) {
-            loadDrawableByGilde(view.context, resId, getTransform(view.context, shape, radius))?.into(view)
-        }
-
-        //從網路撈圖片進 imageView
-        fun load(view: ImageView, url: URL, shape: Shape = Shape.NORMAL, radius: Float? = null) {
-            loadDrawableByGilde(view.context, url.toString(), getTransform(view.context, shape, radius))?.into(view)
-        }
-
-        //從網路撈圖片, 返回指定的圖片, 將圖片設定為指定寬高
-        fun load(context: Context, url: URL, shape: Shape = Shape.NORMAL, radius: Float? = null, handler: (Bitmap) -> Unit) {
-            val target = object: SimpleTarget<Bitmap>() {
-                override fun onResourceReady(resource: Bitmap?, glideAnimation: GlideAnimation<in Bitmap>?) {
-                    if (resource != null) handler(resource)
-                }
-            }
-            loadBitmapByGlide(context, url.toString(), getTransform(context, shape, radius))?.into(target)
-        }
-
-        //傳入uri(本地圖片檔案), 返回指定的圖片, 將圖片設定為指定寬高
-        fun load(context: Context, uri: Uri, shape: Shape = Shape.NORMAL, radius: Float? = null, width: Int? = null, height: Int? = null, handler: (Bitmap) -> Unit) {
-            val target = object: SimpleTarget<Bitmap>() {
-                override fun onResourceReady(resource: Bitmap?, glideAnimation: GlideAnimation<in Bitmap>?) {
-                    if (resource != null) handler(resource)
-                }
-            }
-            loadBitmapByGlide(context, uri, getTransform(context, shape, radius), width, height)?.into(target)
+        fun load(view: ImageView, from: Any, attr: ImageAttr) {
+            loadDrawableByGilde(view.context, from, attr)?.into(view)
         }
 
         private fun getTransform(context: Context, type: Shape, radius: Float? = null): BitmapTransformation? {
@@ -85,33 +63,36 @@ class MGImgLoadUtils {
         }
 
         private fun <T> loadByGlide(context: Context, from: T): DrawableTypeRequest<T>? {
-             val b = when (from) {
+            val b = when (from) {
+                is Int -> Glide.with(context).load(from)
+                is URL -> Glide.with(context).load(from.toString())
                 is Int -> Glide.with(context).load(from)
                 is String -> Glide.with(context).load(from)
-                is Int -> Glide.with(context).load(from)
                 else -> null
             }
             return b as DrawableTypeRequest<T>?
         }
 
-        private fun <T> loadBitmapByGlide(context: Context, from: T, transform: BitmapTransformation? = null, width: Int? = null, height: Int? = null): BitmapRequestBuilder<T, Bitmap>? {
+        private fun <T> loadBitmapByGlide(context: Context, from: T, attr: ImageAttr): BitmapRequestBuilder<T, Bitmap>? {
             val b = loadByGlide(context, from)
             var builder = b?.asBitmap()
                     ?.fitCenter()
                     ?.diskCacheStrategy(DiskCacheStrategy.SOURCE)
                     ?.format(DecodeFormat.PREFER_ARGB_8888)
+            val transform = getTransform(context, attr.shape, attr.radius)
             if (transform != null) {
                 builder = builder?.transform(transform)
             }
-            return overrideSizeByGlide(builder, width, height)
+            return overrideSizeByGlide(builder, attr.width, attr.height)
         }
 
-        private fun <T> loadDrawableByGilde(context: Context, from: T, transform: BitmapTransformation? = null, width: Int? = null, height: Int? = null): DrawableRequestBuilder<T>? {
+        private fun <T> loadDrawableByGilde(context: Context, from: T, attr: ImageAttr): DrawableRequestBuilder<T>? {
             var builder = loadByGlide(context, from)?.fitCenter()
+            val transform = getTransform(context, attr.shape, attr.radius)
             if (transform != null) {
                 builder = builder?.transform(transform)
             }
-            return overrideSizeByGlide(builder, width, height)
+            return overrideSizeByGlide(builder, attr.width, attr.height)
         }
 
         private fun <T> overrideSizeByGlide(builder: DrawableRequestBuilder<T>?, width: Int?, height: Int?): DrawableRequestBuilder<T>? {
@@ -129,6 +110,32 @@ class MGImgLoadUtils {
         }
     }
 
+    class ImageAttr {
+
+        var shape: Shape = Shape.NORMAL
+        var radius: Float = 0f
+        var width: Int? = null
+        var height: Int? = null
+
+        constructor()
+
+        constructor(shape: Shape, radius: Float = 0f) {
+            this.shape = shape
+            this.radius = radius
+        }
+
+        constructor(width: Int?, height: Int?) {
+            this.width = width
+            this.height = height
+        }
+
+        constructor(shape: Shape, radius: Float = 0f, width: Int, height: Int) {
+            this.shape = shape
+            this.radius = radius
+            this.width = width
+            this.height = height
+        }
+    }
 
     enum class Shape {
         CIRCLE, ROUND, NORMAL
